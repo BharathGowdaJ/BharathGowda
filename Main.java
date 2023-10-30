ba60565fcdb7ef775f2f6383b7a3219f583147d5
J M <bharath-gowda.j-m@capgemini.com>
2023-10-30 16:05:39 +0530

    methods are added to classes

--- Main.java
+++ Main.java
@@ -1,52 +1,8 @@
-*Subject - Verb Agreement
+public class Main{
 
-Identify If the sentence is correct or incorrect
-
-Ans:
-1. My friend always lends me money
-2. One hundred mile is a long distance to drive
-3. The more you age the more important is to exercise
-5. Is the news on at five or six ?
-6. Either diana or Mary is helping with stage decorations
-7. Everyone needs a break
-
-
-
-*Tenses:-
-
-
-
-
-A family spending their weekend time or having good time. Everyone are very happy with vacation. 
-Grand parents seems to be extremely happy to spend time with grand children. 
-
-
-
-
-Nasa has launched Mars mission rocket, who ever present in Kennedy city where launch station is present, are taking look at how steadily rocket moving upwards.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public static void main(String[] args) {
+        
+        Hello h = new Hello();
+        h.display();
+    }
+}
